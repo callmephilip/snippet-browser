afcdac30e1b5a833bb54528977b023ada3258251
Philip Nuzhnyi <philip.nuzhnyy@gmail.com>
2023-08-30 13:18:07 +0100

    test public content

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,7 @@
+/* eslint-disable no-console */
+
+import { useEffect } from 'react';
+
 const Link = (props: JSX.IntrinsicElements['a']) => (
   <a
     className="text-pink-500 underline hover:no-underline dark:text-pink-400"
@@ -6,6 +10,21 @@ const Link = (props: JSX.IntrinsicElements['a']) => (
 );
 
 export default function App() {
+  useEffect(() => {
+    const runner = async () => {
+      const r = await fetch('/data/index.json');
+      const data = await r.json();
+
+      console.log('>>>>>>', data);
+
+      const snippet = await (await fetch(`data/${data[0].slug}`)).text();
+
+      console.log('>>>>>>>', snippet);
+    };
+
+    runner();
+  }, []);
+
   return (
     <div className="mx-auto my-8 mt-10 w-8/12 rounded border border-gray-200 p-4 shadow-md dark:border-neutral-600 dark:bg-neutral-800 dark:shadow-none">
       <h1 className="mb-4 text-4xl">Welcome</h1>
